7052ea63fbc3b11044ac4985abba2c1e45f05a22
marcofs10 <121571271+marcofs10@users.noreply.github.com>
2025-02-03 23:35:43 -0500

    feat: added navigation to pokepage

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import * as utils from '../../utils/utils.js'
 import './Pokemon.css'
 
 export const Pokemon = ({ name, id, image, types }) => {
 
+    const navigate = useNavigate()
 
     return (
-        <div className='pokemonThumbnail'>
+        <div className='pokemonThumbnail' onClick={()=>navigate(`/pokedex/pokemon/${id}`)}>
             <div>
                 <img src={image} alt="" />
             </div>
--- src/components/Suggestion/Suggestion.jsx
+++ src/components/Suggestion/Suggestion.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import * as utils from '../../utils/utils.js'
+import { useNavigate } from 'react-router-dom'
 import './Suggestion.css'
 
 export const Suggestion = ({ suggestion, selected, index, setSelectedItem }) => {
 
+    const navigate = useNavigate();
+
     return (
         <div
+            onClick={() => navigate(`/pokedex/pokemon/${utils.extractIdFromURL(suggestion.url)}`)}
             onMouseEnter={() => setSelectedItem(index)}
             className={`suggestion ${selected ? 'selected' : ''}`}>{utils.capFirstLetter(suggestion.name)}
         </div>
